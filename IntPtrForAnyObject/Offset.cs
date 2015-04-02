e1ff0b63539de1a84e41f07a988e651d619cd80e
rusden220 <surin.denis.k@gmail.com>
2015-04-03 01:53:40 +0300

    pointer

---
+++ IntPtrForAnyObject/Offset.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IntPtrForAnyObject
+{
+	[StructLayout(LayoutKind.Explicit)]
+	public class Offset
+	{
+		/// <summary>
+		/// just wrapper
+		/// </summary>
+		public class Field
+		{
+			public object _int;
+		}
+		[FieldOffset(0)]
+		public Field _field;//field for test
+
+		[FieldOffset(0)]
+		public int[] array;//showing ram damp
+
+		public Offset()
+		{
+			int len = 12;
+			array = new int[len];
+			_field = new Field();
+			_field._int = int.MaxValue;
+
+			Pointer pointer = new Pointer(_field._int);
+			IntPtr intPtr = pointer.GetPointer();
+
+			Console.WriteLine("pointer for object " + intPtr.ToString());
+			Console.WriteLine("memory damp");
+			for (int i = 0; i < len; i++)
+			{
+				if (array[i] == intPtr.ToInt32())//if using reference type
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine(i.ToString() + " " + array[i].ToString());
+					Console.ForegroundColor = ConsoleColor.Gray;
+				}
+				else if (array[i] == (int)_field._int)//if using value type
+				{
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine(i.ToString() + " " + array[i].ToString());
+					Console.ForegroundColor = ConsoleColor.Gray;
+				}
+				else
+				{ Console.WriteLine(i.ToString() + " " + array[i].ToString()); }
+			}
+
+		}
+	}
+
+
+}
---
+++ IntPtrForAnyObject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IntPtrForAnyObject
+{
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string str = "Hello, World!";		
+			Pointer pointer = new Pointer(str);
+			IntPtr intPtr = pointer.GetPointer();
+			unsafe //if you any problem try need enable an unsafe code in the project properties
+			{
+				int* x = (int*)pointer.IntPtr.ToInt32();
+				x++;
+				x++;//double increment because type of variable is a reference type
+				Console.WriteLine((char)*x);
+				int num = int.MaxValue;
+				int* y = (int *)pointer.GetPointer(num);
+				y++;//one increment because type of variable is a value type
+				Console.WriteLine(*y);
+			}
+
+			Console.WriteLine("How it looks like inside");
+			new Offset();
+			Console.WriteLine("Press any key");
+			Console.ReadKey();
+		}
+	}
+}
